05b5f6c6f3b78f97cec9f35de4dbe477c1aa4451
Amy <amstarry@gmail.com>
2013-10-03 18:29:23 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kidstagram::Application.config.secret_key_base = '3bc7f4724ed24959ef0c8965109dfd18fdaab38edac0aa349c5d13047b06e13edcd91f5fea84cea3f399e3d4e4c439379b3d210d865799f953fe5ea9e3e38967'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Kidstagram::Application.config.session_store :cookie_store, key: '_kidstagram_session'
